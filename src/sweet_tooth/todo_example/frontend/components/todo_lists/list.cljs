680e749634e97846a00259bfd3ade807810da76a
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2019-12-02 21:38:51 -0500

    link todo lists

--- src/sweet_tooth/todo_example/frontend/components/todo_lists/list.cljs
+++ src/sweet_tooth/todo_example/frontend/components/todo_lists/list.cljs
@@ -1,13 +1,21 @@
 (ns sweet-tooth.todo-example.frontend.components.todo-lists.list
   (:require [re-frame.core :as rf]
-            [sweet-tooth.frontend.form.components :as stfc]))
+            [sweet-tooth.frontend.form.components :as stfc]
+            [sweet-tooth.frontend.routes :as stfr]))
 
 (defn component
   []
   (let [todo-lists @(rf/subscribe [:todo-lists])]
     [:div
      [:div (count todo-lists) " Todo lists"]
+
      (stfc/with-form [:todo-lists :create]
        [:form (on-submit)
         [field :text :todo-list/title]
-        [:input {:type "submit"}]])]))
+        [:input {:type "submit"}]])
+
+     (->> todo-lists
+          (map (fn [tl]
+                 ^{:key (:db/id tl)}
+                 [:div [:a {:href (stfr/path :show-todo-list tl)} (:todo-list/title tl)]]))
+          doall)]))
--- src/sweet_tooth/todo_example/frontend/routes.cljs
+++ src/sweet_tooth/todo_example/frontend/routes.cljs
@@ -2,8 +2,13 @@
   (:require [sweet-tooth.todo-example.frontend.components.todo-lists.list :as tll]))
 
 (def frontend-routes
-  ["/"
-   {:name       :home
-    :lifecycle  {:param-change [:load-todo-lists]}
-    :components {:main [tll/component]}
-    :title      "Todo List"}])
+  [["/"
+    {:name       :home
+     :lifecycle  {:param-change [:load-todo-lists]}
+     :components {:main [tll/component]}
+     :title      "Todo List"}]
+   ["/todo-list/{db/id}"
+    {:name       :show-todo-list
+     :lifecycle  {:param-change [:load-todo-lists]}
+     :components {:main [tll/component]}
+     :title      "Todo List"}]])
