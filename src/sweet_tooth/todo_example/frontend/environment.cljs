fbd4364c238743a30ed591a515f446de321954a2
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2019-12-03 13:11:50 -0500

    whitespace

--- src/sweet_tooth/todo_example/frontend/environment.cljs
+++ src/sweet_tooth/todo_example/frontend/environment.cljs
@@ -5,8 +5,8 @@
   (let [host (u/go-get js/location ["hostname"])
         port (u/go-get js/location ["port"])]
     (cond
-      (= host "localhost")                 (case port
-                                             "4010" :integration
-                                             "3000" :dev
-                                             "3010" :local-staging
-                                             "3100" :local-staging))))
+      (= host "localhost") (case port
+                             "4010" :integration
+                             "3000" :dev
+                             "3010" :local-staging
+                             "3100" :local-staging))))
