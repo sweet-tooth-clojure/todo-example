f715c754ac3397348aca6a6188d5f75da4ddbb21
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2020-05-10 15:04:25 -0400

    use new test helpers

--- test/sweet_tooth/todo_example/backend/endpoint/todo_list_test.clj
+++ test/sweet_tooth/todo_example/backend/endpoint/todo_list_test.clj
@@ -8,32 +8,38 @@
 (use-fixtures :each (eth/system-fixture :test))
 
 (deftest list-todo-lists
-  (let [title         "GET EGGS"
-        {:keys [tl0]} (td/transact! {:todo-list [[1 {:spec-gen {:todo-list/title "GET EGGS"}}]]})
-        resp-data     (-> (eth/req :get "/api/v1/todo-list")
-                          (eth/read-body))]
-    (is (eth/contains-entity? resp-data :todo-list {:todo-list/title title}))))
+  (let [tl        {:todo-list/title "GET EGGS"}
+        _         (td/transact! {:todo-list [[1 {:spec-gen tl}]]})
+        resp-data (-> (eth/req :get "/api/v1/todo-list")
+                      (eth/read-body))]
+    (eth/assert-response-contains-one-entity-like resp-data tl :todo-list)))
 
 
 (deftest shows-todo-list-with-todos
   (let [{:keys [tl0 t0 t1 t2]} (td/transact! {:todo [[5]]})
         resp-data     (-> (eth/req :get (str "/api/v1/todo-list/" tl0))
                           (eth/read-body))]
-    (is (eth/contains-entity? resp-data :todo-list {:db/id tl0}))
-    (is (eth/contains-entity? resp-data :todo {:db/id t0}))
-    (is (eth/contains-entity? resp-data :todo {:db/id t1}))
-    (is (eth/contains-entity? resp-data :todo {:db/id t2}))))
+    (eth/assert-response-contains-entity-like resp-data {:db/id tl0} :todo-list)
+    (eth/assert-response-contains-entity-like resp-data {:db/id t0} :todo)
+    (eth/assert-response-contains-entity-like resp-data {:db/id t1} :todo)
+    (eth/assert-response-contains-entity-like resp-data {:db/id t2} :todo)))
 
 (deftest updates-todo-list
   (let [{:keys [tl0]} (td/transact! {:todo-list [[1 {:spec-gen {:todo-list/title "GET EGGS"}}]]})]
-    (is (eth/contains-entity? (-> (eth/req :put (str "/api/v1/todo-list/" tl0) {:todo-list/title "GET MILK"})
-                                  (eth/read-body))
-                              :todo-list
-                              {:db/id tl0 :todo-list/title "GET MILK"}))
+    (eth/assert-response-contains-one-entity-like (-> (eth/req :put (str "/api/v1/todo-list/" tl0) {:todo-list/title "GET MILK"})
+                                                      (eth/read-body))
+                                                  {:db/id tl0 :todo-list/title "GET MILK"}
+                                                  :todo-list)
     (is (= "GET MILK" (:todo-list/title (d/entity (tdb/db) tl0))))))
 
 (deftest creates-todo-list
   (let [tl            {:todo-list/title "GET EGGS"}
         resp-data     (-> (eth/req :post "/api/v1/todo-list" tl)
                           (eth/read-body))]
-    (is (eth/contains-entity? resp-data :todo-list tl))))
+    (eth/assert-response-contains-one-entity-like resp-data tl :todo-list)))
+
+
+(deftest demo-failing-create-test
+  (let [resp-data (-> (eth/req :post "/api/v1/todo-list" {:todo-list/title "GET EGGS"})
+                      (eth/read-body))]
+    (eth/assert-response-contains-one-entity-like resp-data {:todo-list/title "GET MILK"} :todo-list)))
--- test/sweet_tooth/todo_example/backend/endpoint/todo_test.clj
+++ test/sweet_tooth/todo_example/backend/endpoint/todo_test.clj
@@ -13,14 +13,13 @@
         t             {:todo/title "yeah eggs" :todo/todo-list tl0}
         resp-data     (-> (eth/req :post "/api/v1/todo" t)
                           (eth/read-body))]
-    (is (eth/contains-entity? resp-data :todo (update t :todo/todo-list (fn [id] {:db/id id}))))))
+    (eth/assert-response-contains-entity-like resp-data (update t :todo/todo-list (fn [id] {:db/id id})))))
 
 (deftest updates-todo
   (let [{:keys [t0]} (td/transact! {:todo [[1 {:spec-gen {:todo/title "GET EGGS"}}]]})]
-    (is (eth/contains-entity? (-> (eth/req :put (str "/api/v1/todo/" t0) {:todo/title "GET MILK"})
-                                  (eth/read-body))
-                              :todo
-                              {:db/id t0 :todo/title "GET MILK"}))
+    (eth/assert-response-contains-entity-like (-> (eth/req :put (str "/api/v1/todo/" t0) {:todo/title "GET MILK"})
+                                                  (eth/read-body))
+                                              {:db/id t0 :todo/title "GET MILK"})
     (is (= "GET MILK" (:todo/title (d/entity (tdb/db) t0))))))
 
 (deftest deletes-todo
